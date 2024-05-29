dafe53fa8b5d8aa3aa10ce83f47629087211131a
David Villamizar <davidvilla042211@gmail.com>
2024-05-29 05:38:18 -0500

    modelado

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ df_games = pd.read_parquet(r'Datasets/games.parquet', engine='auto')
 df_reviews = pd.read_parquet(r'Datasets/reviews.parquet', engine='auto')
 df_items = pd.read_parquet(r'Datasets/user_items.parquet', engine='auto')
 
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(df_reviews["review"])
+    
+feactures =  np.column_stack([tfidf_matrix.toarray(), df_reviews["recommend"], df_reviews["sentiment_analysis"]])
+
+similarity_matrix = cosine_similarity(feactures)
+df_sentiment_analysis = df_reviews.reset_index(drop=True)
 
 app = FastAPI( 
     title = 'Machine Learning Operations (MLOps)',
@@ -74,6 +81,7 @@ async def developer(desarrollador : str):
 
 @app.get("/userdata/{user_id}")
 async def userdata( user_id : str ):
+   
     usermask = df_items['user_id'] == user_id
     usermask2 = df_reviews['user_id'] == user_id
     df_user_items = df_items[usermask]
@@ -96,6 +104,7 @@ async def userdata( user_id : str ):
 
 @app.get("/UserForGenre/{genre}")
 async def UserForGenre(genre : str):
+    
     generos = df_games[['item_id','genres']]
     generos['genres'] = generos['genres'].fillna('Unassigned gender')
     df_items2 = pd.merge(df_items, generos, how='left', on=['item_id'], indicator=False)
@@ -109,13 +118,88 @@ async def UserForGenre(genre : str):
 
 @app.get("/best_developer_year/{año}")
 async def best_developer_year (año):
+    
     top = df_reviews[['Year','recommend','developer']]
     mask1 = top['Year'] == año
     top = top[mask1]
     top.drop(columns='Year', inplace=True)
-    agrupados = top.groupby('developer')['recommend'].sum().sort_values(ascending=False)
-    top1 = agrupados[0:1].idxmax()
-    top2 = agrupados[0:2].idxmax()
-    top_3= agrupados[0:3].idxmax()
+    agrupados = top.groupby('developer')['recommend'].sum().sort_values(ascending=False).head(3).to_dict()
+    
+    return agrupados
 
-    return f'Puesto 1: {top1}, Puesto 2: {top2}, Puesto 3: {top_3}'
+@app.get("/develreviewsanalys/({desarrolladorsent})")
+def devel_reviews_analys(desarrollador: str):
+
+    mask1 = df_reviews['developer'] == desarrollador
+    df = df_reviews[mask1]
+    comentario = df['Sentiment_analysis'].value_counts().to_dict()
+    comentario['Positivo'] = comentario.pop(2)
+    comentario['Neutro'] = comentario.pop(1)
+    comentario['Negativo'] = comentario.pop(0)
+    diccionario = {desarrollador: comentario}
+    
+    return diccionario
+
+
+# ML: RECOMENDACIÓN USER-ITEM:
+
+@app.get("/recomendacion_juego/{item_id}",  tags=['recomendacion'])
+async def recomendacion_juego (item_id : int):
+    '''
+    <strong>Devuelve una cantidad de 5 juegos recomendado a partir del identifcador de un juego</strong>
+             
+    Parametro
+    ---------
+             item_id : Identificador unico del juego.
+    
+    Retorna
+    -------   
+             Diccionario con una lista de 5 juegos similiares recomendados a partir del ingresado
+    '''
+
+    producto = df_sentiment_analysis[df_sentiment_analysis['item_id'] == item_id]
+    if not producto.empty:
+        product_index = producto.index[0]
+        product_similarities = similarity_matrix[product_index]
+        most_similar_products_indices = np.argsort(-product_similarities)
+        most_similar_products = df_sentiment_analysis.loc[most_similar_products_indices, 'item_name']
+    else:
+        return "Producto no encontrado"
+    
+    diccionario = {"Juegos recomendados" : list()}
+    similares = most_similar_products[:5]
+    diccionario["Juegos recomendados"] = [similar for similar in similares]
+    diccionario
+
+    return diccionario
+
+# Endpoint http://127.0.0.1:8000/recomendacion_usuario/{user_id}
+@app.get("/recomendacion_usuario/{user_id}",  tags=['recomendacion'])
+async def recomendacion_usuario (user_id):
+    '''
+    <strong>Devuelve una cantidad de 5 juegos recomendado a partir del identifcador unico de un usuario</strong>
+             
+    Parametro
+    ---------
+             user_id : Identificador unico del juego.
+    
+    Retorna
+    -------   
+             Diccionario con una lista de 5 juegos similares recomendados por un usuario
+    '''
+
+    producto = df_sentiment_analysis[df_sentiment_analysis['user_id'] == user_id]
+    if not producto.empty:
+        product_index = producto.index[0]
+        product_similarities = similarity_matrix[product_index]
+        most_similar_products_indices = np.argsort(-product_similarities)
+        most_similar_products = df_sentiment_analysis.loc[most_similar_products_indices, 'item_name']
+    else:
+        return "Producto no encontrado"
+    
+    diccionario = {"Juegos recomendados" : list()}
+    similares = most_similar_products[:5]
+    diccionario["Juegos recomendados"] = [similar for similar in similares]
+    diccionario
+
+    return diccionario
